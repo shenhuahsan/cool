4bc2c43a2ada4daaebaca0408ef7495a590f67e3
shenhuashan <15836216972@163.com>
2016-09-19 17:19:21 +0800

    first commit

--- a.c
+++ a.c
@@ -3,4 +3,15 @@
 int main()
 {
 	return 0;
+
+
+
+
+echo "# cool" >> a.c
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/shenhuahsan/cool.git
+git push -u origin master
 }
+# cool
